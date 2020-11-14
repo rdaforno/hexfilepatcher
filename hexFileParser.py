f196dec9a0febb3010faf6b9123696a349980430
rdaforno <keepcoding@gmx.net>
2020-11-14 10:24:29 +0100

    renaming / reordering / bugfix

---
+++ hexFileParser.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python3
+
+######################################################################################
+#
+# Intel HEX file parser
+#
+# 2020, rdaforno
+#
+######################################################################################
+
+
+import binascii
+
+
+# Intel HEX file parser
+class HexFileParser:
+  lines = []
+
+  def __init__(self, filename):
+    self.lines.clear()
+    self.load(filename)
+
+  def load(self, filename):
+    self.lines.clear()
+    with open(filename) as fp:
+      for line in fp:
+        if line[0] != ':':
+          print("line '%s' skipped.." % line)
+          continue
+        line = line[1:].strip()
+        l = int(line[0:2], 16)
+        addr = int(line[2:6], 16)
+        t = int(line[6:8], 16)
+        data = line[8:-2]
+        if len(data) != (l * 2):
+          print("invalid data length! line '%s' skipped.." % line)
+          continue
+        crc = int(line[-2:], 16)
+        if self.calc_line_crc(line[:-2]) != crc:
+          print("invalid hex file, CRC doesn't match!")
+          break
+        self.lines.append({ 'len':l, 'addr':addr, 'type':t, 'data':data, 'crc':crc })
+    print("%u lines loaded from %s" % (len(self.lines), filename))
+
+  def print_lines(self):
+    for line in self.lines:
+      print("length: %u, address: 0x%02x, type: %u, data: %s, crc: %02x" % (line['len'], line['addr'], line['type'], line['data'], line['crc']))
+
+  def save(self, filename):
+    fp = open(filename, "w")
+    if fp:
+      for line in self.lines:
+        fp.write(":%02X%04X%02X%s%02X\n" % (line['len'], line['addr'], line['type'], line['data'], line['crc']))
+      fp.close()
+      print("hex file saved as %s" % filename)
+
+  def save_as_c_var(self, filename):
+    fp = open(filename, "w")
+    if fp:
+      fp.write("const char hex_image[] = {\n  \"")
+      fp.write("\"\n  \"".join(self.serialize_data()))
+      fp.write("\"\n};\n")
+      fp.close()
+      print("hex file saved as %s" % filename)
+
+  def save_as_binary(self, filename):
+    fp = open(filename, "wb")
+    if fp:
+      fp.write(bytes.fromhex("".join(self.serialize_data())))
+      fp.close()
+      print("binary file saved as %s" % filename)
+
+  def calc_crc32(self):
+    return "0x%x" % binascii.crc32(bytes.fromhex("".join(self.serialize_data())))
+
+  def serialize_data(self, start_addr=0, line_width=64):
+    serialized_data = []
+    curr_ofs = 0
+    for line in self.lines:
+      if line['type'] == 0:
+        curr_addr = curr_ofs + line['addr']
+        if curr_addr > start_addr:
+          serialized_data.append('00'*(curr_addr - start_addr))  # fill gap with zeros
+          print("added %d padding bytes at address 0x%x" % (curr_addr - start_addr, curr_addr))
+        serialized_data.append(line['data'])
+        start_addr = curr_addr + line['len']
+      elif line['type'] == 4:
+        curr_ofs = int(line['data'][0:4], 16) * 65536
+        if start_addr == 0:
+          # if this is the first line and start_addr is not given, then use this offset as the start address
+          start_addr = curr_ofs
+          print("start address set to 0x%x" % start_addr)
+        else:
+          print("address offset found: 0x%x" % curr_ofs)
+        if curr_ofs < start_addr:
+          print("invalid address offset")
+          return None
+      else:
+        print("skipping line of type %u" % line['type'])
+    serialized_str = "".join(serialized_data)
+    print("binary size is %u bytes" % (len(serialized_str) / 2))
+    if line_width == 0:
+      return serialized_str
+    else:
+      return [serialized_str[i:i+line_width] for i in range(0, len(serialized_str), line_width)]
+
+  def addr_to_lineno(self, addr):
+    for i in range(len(self.lines)):
+      if self.lines[i]['type'] != 0:    # only look at data records
+        continue
+      if self.lines[i]['addr'] <= addr and (self.lines[i]['addr'] + self.lines[i]['len']) > addr:
+        return i
+    return -1
+
+  def replace_data(self, addr, size, data):
+    if size != 1 and size != 2 and size != 4:
+      print("size %d is not supported" % size)
+      return 0
+    i = self.addr_to_lineno(addr)
+    if i >= 0:
+      ofs = (addr - self.lines[i]['addr'])
+      if (addr + size) > (self.lines[i]['addr'] + self.lines[i]['len']):   # data stretches over 2 lines
+        # make sure there is no jump in address to the next line
+        if (i+1) == len(self.lines) or ((self.lines[i+1]['addr'] - self.lines[i]['addr']) > self.lines[i]['len']):
+          print("out of bound error")
+          return 0
+        self.lines[i]['data'] = self.insert_data(self.lines[i]['data'], ofs, self.lines[i]['len'] - ofs, data)
+        self.lines[i+1]['data'] = self.insert_data(self.lines[i+1]['data'], 0, size - (self.lines[i]['len'] - ofs), data)
+        self.update_line_crc(i+1)
+      else:
+        self.lines[i]['data'] = self.insert_data(self.lines[i]['data'], (addr - self.lines[i]['addr']), size, data)
+      self.update_line_crc(i)
+      return 1
+    else:
+      return 0
+
+  def insert_data(self, line, ofs, size, data):  # inserts 'data' of length 'size' into 'line' at offset 'ofs'
+    if size == 1:
+      return line[:ofs*2] + ("%02X" % (data % 256)) + line[(ofs+size)*2:]
+    elif size == 2:
+      return line[:ofs*2] + ("%02X%02X" % (data % 256, (data >> 8) % 256)) + line[(ofs+size)*2:]             # little endian!
+    elif size == 4:
+      return line[:ofs*2] + ("%02X%02X%02X%02X" % (data % 256, (data >> 8) % 256, (data >> 16) % 256, (data >> 24) % 256)) + line[(ofs+size)*2:]    # little endian!
+
+  def update_line_crc(self, idx):
+    if idx < len(self.lines):
+      self.lines[idx]['crc'] = self.calc_line_crc("%02X%04X%02X%s" % (self.lines[idx]['len'], self.lines[idx]['addr'], self.lines[idx]['type'], self.lines[idx]['data']))
+
+  def calc_line_crc(self, line):
+    crc = 0
+    l = 0
+    while l < len(line) - 1:
+      crc = crc + int(line[l:l+2], 16)
+      l = l + 2
+    crc = (~crc + 1) % 256
+    return crc
--- patchHexFile.py
+++ patchHexFile.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 ######################################################################################
 #
@@ -19,134 +19,16 @@ import sys
 import os.path
 import xml.etree.ElementTree as ET
 import binascii
+import hexFileParser
 
 
 xmlFile = 'fwConfig.xml'    # default filename
 
 
 def usage():
-  print("\r\nusage:  %s [firmware] [config]\r\n\r\n"
-        "  firmware \t filename of the firmware (Intel hex format)\r\n"
+  print("\r\nusage:  %s [filename] [config]\r\n\r\n"
+        "  filename \t path to an Intel hex file\r\n"
         "  config \t (optional) filename of the XML config; if not provided, 'fwConfig.xml' will be used\r\n" % sys.argv[0])
-
-# Intel HEX file parser
-class hexFileParser:
-  lines = []
-
-  def __init__(self, fileName):
-    self.loadFromFile(fileName)
-
-  def loadFromFile(self, fileName):
-    with open(fileName) as fp:
-      for line in fp:
-        if line[0] != ':':
-          print("line '%s' skipped.." % line)
-          continue
-        line = line[1:].strip()
-        l = int(line[0:2], 16)
-        addr = int(line[2:6], 16)
-        t = int(line[6:8], 16)
-        data = line[8:-2]
-        if len(data) != (l * 2):
-          print("invalid data length! line '%s' skipped.." % line)
-          continue
-        crc = int(line[-2:], 16)
-        if self.calcLineCRC(line[:-2]) != crc:
-          print("invalid hex file, CRC doesn't match!")
-          break
-        self.lines.append({ 'len':l, 'addr':addr, 'type':t, 'data':data, 'crc':crc })
-    print("%u lines loaded from %s" % (len(self.lines), fileName))
-
-  def printLines(self):
-    for line in self.lines:
-      print("length: %u, address: 0x%02x, type: %u, data: %s, crc: %02x" % (line['len'], line['addr'], line['type'], line['data'], line['crc']))
-
-  def saveAsFile(self, fileName):
-    fp = open(fileName, "w")
-    if fp:
-      for line in self.lines:
-        fp.write(":%02X%04X%02X%s%02X\n" % (line['len'], line['addr'], line['type'], line['data'], line['crc']))
-      fp.close()
-      print("hex file saved as %s" % fileName)
-
-  def saveAsCVariable(self, fileName):
-    fp = open(fileName, "w")
-    if fp:
-      fp.write("const char hex_image[] = {\n  \"")
-      fp.write("\"\n  \"".join(self.serializeData()))
-      fp.write("\"\n};\n")
-      fp.close()
-      print("hex file saved as %s" % fileName)
-
-  def calcDataCRC32(self):
-    return "0x%x" % binascii.crc32(bytes.fromhex("".join(self.serializeData())))
-
-  def serializeData(self, start_addr=0, line_width=64):
-    serialized_data = []
-    for line in self.lines:
-      if line['addr'] > start_addr:
-        # fill gap with zeros
-        num_padding_bytes = (line['addr'] - start_addr)
-        serialized_data.append('00'*(num_padding_bytes))
-        print("added %d padding bytes at address %x" % (num_padding_bytes, line['addr']))
-      serialized_data.append(line['data'])
-      start_addr = line['addr'] + line['len']
-    serialized_str = "".join(serialized_data)
-    if line_width == 0:
-      return serialized_str
-    else:
-      return [serialized_str[i:i+line_width] for i in range(0, len(serialized_str), line_width)]
-
-  def addrToLineNo(self, addr):
-    for i in range(len(self.lines)):
-      if self.lines[i]['type'] != 0:    # only look at data records
-        continue
-      if self.lines[i]['addr'] <= addr and (self.lines[i]['addr'] + self.lines[i]['len']) > addr:
-        return i
-    return -1
-
-  def replaceData(self, addr, size, data):
-    if size != 1 and size != 2 and size != 4:
-      print("size %d is not supported" % size)
-      return 0
-    i = self.addrToLineNo(addr)
-    if i >= 0:
-      ofs = (addr - self.lines[i]['addr'])
-      if (addr + size) > (self.lines[i]['addr'] + self.lines[i]['len']):   # data stretches over 2 lines
-        # make sure there is no jump in address to the next line
-        if (i+1) == len(self.lines) or ((self.lines[i+1]['addr'] - self.lines[i]['addr']) > self.lines[i]['len']):
-          print("out of bound error")
-          return 0
-        self.lines[i]['data'] = self.insertData(self.lines[i]['data'], ofs, self.lines[i]['len'] - ofs, data)
-        self.lines[i+1]['data'] = self.insertData(self.lines[i+1]['data'], 0, size - (self.lines[i]['len'] - ofs), data)
-        self.updateLineCRC(i+1)
-      else:
-        self.lines[i]['data'] = self.insertData(self.lines[i]['data'], (addr - self.lines[i]['addr']), size, data)
-      self.updateLineCRC(i)
-      return 1
-    else:
-      return 0
-
-  def insertData(self, line, ofs, size, data):  # inserts 'data' of length 'size' into 'line' at offset 'ofs'
-    if size == 1:
-      return line[:ofs*2] + ("%02X" % (data % 256)) + line[(ofs+size)*2:]
-    elif size == 2:
-      return line[:ofs*2] + ("%02X%02X" % (data % 256, (data >> 8) % 256)) + line[(ofs+size)*2:]             # little endian!
-    elif size == 4:
-      return line[:ofs*2] + ("%02X%02X%02X%02X" % (data % 256, (data >> 8) % 256, (data >> 16) % 256, (data >> 24) % 256)) + line[(ofs+size)*2:]    # little endian!
-
-  def updateLineCRC(self, idx):
-    if idx < len(self.lines):
-      self.lines[idx]['crc'] = self.calcLineCRC("%02X%04X%02X%s" % (self.lines[idx]['len'], self.lines[idx]['addr'], self.lines[idx]['type'], self.lines[idx]['data']))
-
-  def calcLineCRC(self, line):
-    crc = 0
-    l = 0
-    while l < len(line) - 1:
-      crc = crc + int(line[l:l+2], 16)
-      l = l + 2
-    crc = (~crc + 1) % 256
-    return crc
 
 
 def parseXML(xmlFile, hexFile):
@@ -164,7 +46,7 @@ def parseXML(xmlFile, hexFile):
             val = int(var.text, 16)
           else:
             val = int(var.text)
-          if hexFile.replaceData(ofs, size, val):
+          if hexFile.replace_data(ofs, size, val):
             print("%u bytes at address 0x%04X overwritten with value %d" % (size, ofs, val))
           else:
             print("failed to write to address 0x%04X" % (ofs))
@@ -176,9 +58,9 @@ if __name__== "__main__":
   if len(sys.argv) < 2:
     usage()
     sys.exit(1)
-  hexFileName = sys.argv[1]      # first argument is the hex filename
-  if not os.path.isfile(hexFileName) or not hexFileName.lower().endswith('hex'):
-    print("invalid file '%s'" % hexFileName)
+  hex_filename = sys.argv[1]      # first argument is the hex filename
+  if not os.path.isfile(hex_filename) or not hex_filename.lower().endswith('hex'):
+    print("invalid file '%s'" % hex_filename)
     sys.exit(1)
   if len(sys.argv) > 2:
     xmlFile = sys.argv[2]   # second argument is the config filename
@@ -186,8 +68,7 @@ if __name__== "__main__":
     print("invalid file '%s'" % xmlFile)
     sys.exit(1)
   # load the hex file
-  hexFile = hexFileParser(hexFileName)
+  hf = hexFileParser.HexFileParser(hex_filename)
   # parse and apply the XML config
-  parseXML(xmlFile, hexFile)
-  hexFile.saveAsFile("parsed_" + hexFileName)
-
+  parseXML(xmlFile, hf)
+  hf.save("parsed_" + hex_filename)
